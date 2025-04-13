e433d4147198eeb840f09b72dab21a747c128681
eBregasi <ermalbregasi@gmail.com>
2025-04-13 10:34:44 +0200

    Ndryshimi i settings Debug

--- rentify/settings.py
+++ rentify/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0h-or$eckt#i(@q*ecwoizhqsxo#(qq1-(hc4gy1so+=*h4*6^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
